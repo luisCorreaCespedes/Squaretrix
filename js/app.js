fad48016dc3700d3c8de90fcf8b84df742b76ae9
Luis Correa <luiscorreacespedes@gmail.com>
2021-05-25 21:59:02 -0400

    Background added

--- js/app.js
+++ js/app.js
@@ -3,3 +3,27 @@ for (let i = 0; i < 200; i++) {
     let block = '<div class="block"></div>'
     board.innerHTML += block;
 }
+
+falls = () => {
+    let fall_count = 50;
+    let container = document.querySelector('.container');
+    for (let i = 0; i < fall_count; i++) {
+        let img_index = Math.floor(Math.random() * 7) + 1;
+        let x = Math.floor(Math.random() * window.innerWidth);
+        let y = Math.floor(Math.random() * window.innerHeight);
+        let size = Math.random() * 40;
+        let duration = Math.random() * 70 + 30;
+        let img = document.createElement('img');
+        img.className = 'fall';
+        img.src = 'assets/images/' + img_index + '.png';
+        img.style.width = 1 + size + 'px';
+        img.style.height = 'auto';
+        img.style.left = x + 'px';
+        img.style.bottom = y + 'px';
+        img.style.animationDuration = 2 + duration + 's';
+        img.style.animationDelay = -duration + 's';
+        container.appendChild(img); 
+    }
+}
+
+falls();
--- js/game.js
+++ js/game.js
@@ -7,10 +7,23 @@ btns.forEach(e => {
         switch(btn_id) {
             case 'btn-play':
                 body.classList.add('play');
+                if (body.classList.contains('pause')){
+                    body.classList.remove('pause');
+                }
                 break;
             case 'btn-theme':
                 body.classList.toggle('dark');
                 break;
+            case 'btn-pause':
+                let btn_play = document.querySelector('#btn-play');
+                btn_play.innerHTML = 'Continuar';
+                body.classList.remove('play');
+                body.classList.add('pause');
+                break;   
+            case 'btn-new-game':
+                body.classList.add('play');
+                body.classList.remove('pause');
+                break; 
         }
     })
 })
